7126e01dcdf0a186bbca60aa24b736a367039f81
Yuki Hattori <yuki.hattori@speee.jp>
2019-04-24 14:04:49 +0900

    Support title and description directives

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,26 @@
+const { Marp } = require('@marp-team/marp-core')
+const fs = require('fs')
+
+// Base URL
+let publicPath = process.env.URL || '/'
+if (!publicPath.endsWith('/')) publicPath += '/'
+
+// Parse title and description from Markdown
+const marp = new Marp()
+
+marp.customDirectives.global.title = title => ({ title })
+marp.customDirectives.global.description = description => ({ description })
+marp.render(fs.readFileSync(`${__dirname}/public/slide.md`).toString())
+
+const title = marp.lastGlobalDirectives.title || 'Marp Vue Slide'
+const description = marp.lastGlobalDirectives.description || 'Marp Vue Slide'
+
 module.exports = {
+  publicPath,
   lintOnSave: false,
   pages: {
-    index: 'src/main.js',
-    presenter: 'src/presenter.js',
+    index: { entry: 'src/main.js', title, description },
+    presenter: { entry: 'src/presenter.js', title },
   },
   chainWebpack: config =>
     config.module
